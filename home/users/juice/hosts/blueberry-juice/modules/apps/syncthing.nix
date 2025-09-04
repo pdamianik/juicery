872c62869c489d6938edbcb1f5caa44dde9981fb
pdamianik <39028343+pdamianik@users.noreply.github.com>
2025-09-04 19:38:46 +0200

    Upsies, wrong line diff

--- home/users/juice/hosts/blueberry-juice/modules/apps/syncthing.nix
+++ home/users/juice/hosts/blueberry-juice/modules/apps/syncthing.nix
@@ -1,5 +1,5 @@
 { config, host, ... }:
 {
   services.syncthing.key = config.sops.secrets."hosts/${host}/syncthing/key".path;
-  services.syncthing.cert = config.sops.secrets."hosts/${host}/syncthing/key".cert;
+  services.syncthing.cert = config.sops.secrets."hosts/${host}/syncthing/cert".path;
 }
