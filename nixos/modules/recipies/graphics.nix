3236da88069d1f00f8a6dc3473070105f8164041
pdamianik <39028343+pdamianik@users.noreply.github.com>
2025-10-26 18:16:10 +0100

    Add drawing tablet driver

--- nixos/modules/recipies/graphics.nix
+++ nixos/modules/recipies/graphics.nix
@@ -1,6 +1,12 @@
+{ pkgs, ... }:
+let
+  xp-pen-01-v3 = pkgs.callPackage ../../../pkgs/xp-pen-01-v3/package.nix {};
+in
 {
   hardware.opentabletdriver = {
     enable = true;
     daemon.enable = true;
   };
+
+  environment.systemPackages = [ xp-pen-01-v3 ];
 }
---
+++ pkgs/xp-pen-01-v3/package.nix
@@ -0,0 +1,83 @@
+{
+  lib,
+  stdenv,
+  fetchzip,
+  libusb1,
+  glibc,
+  libGL,
+  makeWrapper,
+  libsForQt5,
+  # qtx11extras,
+  # wrapQtAppsHook,
+  autoPatchelfHook,
+  libX11,
+  libXtst,
+  libXi,
+  libXrandr,
+  libXinerama,
+}:
+
+let
+  dataDir = "var/lib/xppend1v3";
+in
+stdenv.mkDerivation rec {
+  pname = "xp-pen-deco-01-v2-driver";
+  version = "4.0.7-250117";
+
+  src = fetchzip {
+    url = "https://download01.xp-pen.com/file/2025/02/XPPenLinux${version}.tar.gz";
+    name = "xp-pen-deco-01-v3-driver-${version}.tar.gz";
+    sha256 = "sha256-sH05Qquo2u0npSlv8Par/mn1w/ESO9g42CCGwBauHhU=";
+  };
+
+  nativeBuildInputs = [
+    libsForQt5.wrapQtAppsHook
+    autoPatchelfHook
+    makeWrapper
+  ];
+
+  dontBuild = true;
+
+  dontWrapQtApps = true; # this is done manually
+
+  buildInputs = [
+    libusb1
+    libX11
+    libXtst
+    libXi
+    libXrandr
+    libXinerama
+    glibc
+    libGL
+    (lib.getLib stdenv.cc.cc)
+    libsForQt5.qtx11extras
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/{opt,bin}
+    cp -r App/usr/lib/pentablet/{PenTablet,conf} $out/opt
+    chmod +x $out/opt/PenTablet
+    cp -r App/lib $out/lib
+    sed -i 's#usr/lib/pentablet#${dataDir}#g' $out/opt/PenTablet
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    makeWrapper $out/opt/PenTablet $out/bin/xp-pen-deco-01-v3-driver \
+      "''${qtWrapperArgs[@]}" \
+      --run 'if [ "$EUID" -ne 0 ]; then echo "Please run as root."; exit 1; fi' \
+      --run 'if [ ! -d /${dataDir} ]; then mkdir -p /${dataDir}; cp -r '$out'/opt/conf /${dataDir}; chmod u+w -R /${dataDir}; fi'
+  '';
+
+  meta = with lib; {
+    homepage = "https://www.xp-pen.com/download/deco-01-v3.html";
+    description = "Drivers for the XP-PEN Deco 01 v3 drawing tablet";
+    platforms = [ "x86_64-linux" ];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    maintainers = with maintainers; [ virchau13 ];
+    license = licenses.unfree;
+  };
+}
